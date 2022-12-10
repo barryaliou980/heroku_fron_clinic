8d079b66741a33169df24aed69f0cbda4c3d6326
Aliou Barry <barryaliou980@gmail.com>
2022-12-10 19:42:54 +0000

    test

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const serveStatic = require('serve-static'),
+const express = require('express'),
+  serveStatic = require('serve-static'),
   history = require('connect-history-api-fallback'),
   port = process.env.PORT || 5000;
-;
+
 const app = express();
-;
+
 app.use(history());
-app.use(serveStati;c(__dirname + '/dist/spa'));
-app.listen(port);;
-;
+app.use(serveStatic(__dirname + '/dist/spa'));
+app.listen(port);
